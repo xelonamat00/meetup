29398740fa52a726bc592af5dc8314d9327c542a
faisal <abugeneration@yahoo.co.id>
2018-10-25 06:45:20 +0700

    finish auth

--- src/main.js
+++ src/main.js
@@ -2,15 +2,29 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import {store} from './store'
+import AlerCmp from './components/Shared/Alert.vue'
+import './components/stylus/main.css'
 // import DateFilter from './filters/date'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#D32F2F',
+    accent: '#FF5252',
+    secondary: '#BDBDBD',
+    info: '#1E88E5',
+    warning: '#FFA000',
+    error: '#D50000',
+    success: '#66BB6A'
+  }
+})
 // Vue.filter('date', DateFilter)
 Vue.config.productionTip = false
+Vue.component('app-alert', AlerCmp)
 
 /* eslint-disable no-new */
 new Vue({
@@ -18,5 +32,14 @@ new Vue({
   router,
   store,
   components: { App },
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBkVxrlnxoEDn752LW286cHl8P1PUM6KHA',
+      authDomain: 'meetup-tugasbudesi.firebaseapp.com',
+      databaseURL: 'https://meetup-tugasbudesi.firebaseio.com',
+      projectId: 'meetup-tugasbudesi',
+      storageBucket: 'meetup-tugasbudesi.appspot.com'
+    })
+  },
   template: '<App/>'
 })
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as firebase from 'firebase'
 
 Vue.use(Vuex)
 
@@ -20,14 +21,25 @@ export const store = new Vuex.Store({
         location: 'Tangerang',
         description: 'ngapain aja'}
     ],
-    user: {
-      id: 'dfsdf324',
-      registeredMeetups: ['kjsdfkjsdfk3']
-    }
+    user: null,
+    loading: false,
+    error: null
   },
   mutations: {
     createMeetup (state, payload) {
       state.loadedMeetups.push(payload)
+    },
+    setUser (state, payload) {
+      state.user = payload
+    },
+    setLoading (state, payload) {
+      state.loading = payload
+    },
+    setError (state, payload) {
+      state.error = payload
+    },
+    clearError (state) {
+      state.error = null
     }
   },
   actions: {
@@ -43,6 +55,53 @@ export const store = new Vuex.Store({
       }
       // reachout to firebase and store it
       commit('createMeetup', meetup)
+    },
+    signUserUp ({commit}, payload) {
+      commit('setLoading', true)
+      commit('clearError')
+      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+      .then(
+        user => {
+          commit('setLoading', false)
+          const newUser = {
+            id: user.user.uid,
+            registeredMeetups: []
+          }
+          commit('setUser', newUser)
+        }
+      )
+      .catch(
+        error => {
+          commit('setLoading', false)
+          commit('setError', error)
+          console.log(error)
+        }
+      )
+    },
+    signUserIn ({commit}, payload) {
+      commit('setLoading', true)
+      commit('clearError')
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+      .then(
+        user => {
+          commit('setLoading', false)
+          const newUser = {
+            id: user.user.uid,
+            registeredMeetups: []
+          }
+          commit('setUser', newUser)
+        }
+      )
+      .catch(
+        error => {
+          commit('setLoading', false)
+          commit('setError', error)
+          console.log(error)
+        }
+      )
+    },
+    clearError ({commit}) {
+      commit('clearError')
     }
   },
   getters: {
@@ -61,6 +120,15 @@ export const store = new Vuex.Store({
           return parseInt(meetup.id) === parseInt(meetupId)
         })
       }
+    },
+    user (state) {
+      return state.user
+    },
+    loading (state) {
+      return state.loading
+    },
+    error (state) {
+      return state.error
     }
   }
 })
